55eb1d858886baaa08c62648d22f03ba973a9a0e
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-08-30 10:00:56 +0200

    commiit lint changes

--- functions/index.js
+++ functions/index.js
@@ -1,155 +1,144 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const User = require('./models/user.models')
-const morgan = require('morgan')
-const decodeIDToken = require('./authenticateToken');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.models");
+const morgan = require("morgan");
+const decodeIDToken = require("./authenticateToken");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 
 app.use(cors({
-  origin: 'booking-hotel-25ea1.firebaseapp.com', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: "booking-hotel-25ea1.firebaseapp.com",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 }));
 app.use(express.json());
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 app.use(decodeIDToken);
 
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://hoteldev-724e1.firebaseio.com' 
+  databaseURL: "https://hoteldev-724e1.firebaseio.com",
 });
 
 
-mongoose.connect(functions.config().mongodb.uri,{
+mongoose.connect(functions.config().mongodb.uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}) 
-//mongoose.connect('mongodb://127.0.0.1:27017/hotel');
-const stripe = require("stripe")(functions.config().stripe.privatekey) 
+});
+// mongoose.connect('mongodb://127.0.0.1:27017/hotel');
+const stripe = require("stripe")(functions.config().stripe.privatekey);
 
-const UserData = require('./models/user.models');
-const Rooms = require('./models/hotel.models')
+const UserData = require("./models/user.models");
+const Rooms = require("./models/hotel.models");
 
-app.post('/api/signup', async (req, res) => {
+app.post("/api/signup", async (req, res) => {
   try {
-  
-    await User.create({ 
+    await User.create({
       uid: req.body.uid,
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
       passwordRepeat: req.body.passwordRepeat,
     });
-  
-    res.json({ status: 'ok' });
+
+    res.json({status: "ok"});
   } catch (err) {
     if (err.code === 11000) {
-      res.json({ status: 'error', error: 'Email already exists' });
+      res.json({status: "error", error: "Email already exists"});
     } else {
-      res.json({ status: 'error', error: 'An error occurred' });
+      res.json({status: "error", error: "An error occurred"});
     }
-  }})
+  }
+});
 
-app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body;
+app.post("/api/login", async (req, res) => {
+  const {email, password} = req.body;
 
   try {
-    const user = await User.findOne({ 
+    const user = await User.findOne({
       email: email,
       password: password,
     });
 
     if (!user) {
-      return res.json({ status: 'error', user: false });
+      return res.json({status: "error", user: false});
     }
 
-    return res.json({ user: true });
+    return res.json({user: true});
   } catch (err) {
-    return res.json({ status: 'error', user: false });
+    return res.json({status: "error", user: false});
   }
 });
 
-app.get('/api/all-rooms', async (req, res) => {
-  
+app.get("/api/all-rooms", async (req, res) => {
   try {
     const allRooms = await Rooms.find({});
-    const roomPicture = allRooms.map(room => ({       
-      roomPhoto: room.RoomPhoto
+    const roomPicture = allRooms.map((room) => ({
+      roomPhoto: room.RoomPhoto,
     }));
-    res.json({ pictures: roomPicture });
+    res.json({pictures: roomPicture});
   } catch (error) {
-    console.error('Error fetching all rooms:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching all rooms:", error);
+    res.status(500).json({message: "Internal server error"});
   }
-
 });
 
-app.get('/api/booked-rooms', async (req, res) => {
-
- 
-  
+app.get("/api/booked-rooms", async (req, res) => {
   try {
-    const bookedRooms = await Rooms.find({ isBooked: true }, 'room price RoomPhoto');
-    
-    const roomInfo = bookedRooms.map(room => ({ 
+    const bookedRooms = await Rooms.find({isBooked: true}, "room price RoomPhoto");
+
+    const roomInfo = bookedRooms.map((room) => ({
       roomNumber: room.room,
       price: room.preice,
-      roomPhoto: room.RoomPhoto 
+      roomPhoto: room.RoomPhoto,
     }));
-    
+
     res.json({
-      message: 'Booked rooms',
-      bookedRooms: roomInfo
+      message: "Booked rooms",
+      bookedRooms: roomInfo,
     });
   } catch (error) {
-    console.error('Error fetching booked rooms:', error); 
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching booked rooms:", error);
+    res.status(500).json({message: "Internal server error"});
   }
-
 });
 
-app.get('/api/available-rooms', async (req, res) => {
-
-  
+app.get("/api/available-rooms", async (req, res) => {
   try {
-    const availableRooms = await Rooms.find({ isBooked: false }, 'room rentperday RoomPhoto');
-    
-    const roomInfo = availableRooms.map(room => ({
+    const availableRooms = await Rooms.find({isBooked: false}, "room rentperday RoomPhoto");
+
+    const roomInfo = availableRooms.map((room) => ({
       roomNumber: room.room,
       rentPerDay: room.rentperday,
-      roomPhoto: room.RoomPhoto 
+      roomPhoto: room.RoomPhoto,
     }));
-    
+
     res.json({
-      message: 'Available rooms',
-      availableRooms: roomInfo
+      message: "Available rooms",
+      availableRooms: roomInfo,
     });
   } catch (error) {
-    console.error('Error fetching available rooms:', error); 
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching available rooms:", error);
+    res.status(500).json({message: "Internal server error"});
   }
-
 });
 
-app.get('/room/:roomNumber', async (req, res) => {
-
-
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-res.setHeader('Pragma', 'no-cache');
-res.setHeader('Expires', '0');
-  const { roomNumber } = req.params;
+app.get("/room/:roomNumber", async (req, res) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  const {roomNumber} = req.params;
 
   try {
-    const room = await Rooms.findOne({ room: roomNumber });  
+    const room = await Rooms.findOne({room: roomNumber});
     if (!room) {
-      return res.status(404).json({ message: 'Room not found' });
+      return res.status(404).json({message: "Room not found"});
     }
 
     res.json({
@@ -157,85 +146,79 @@ res.setHeader('Expires', '0');
       roomType: room.roomType,
       description: room.discription,
       roomPhoto: room.RoomPhoto,
-      rentPerDay: room.rentperday
+      rentPerDay: room.rentperday,
     });
   } catch (error) {
-    console.error('Error fetching room information:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching room information:", error);
+    res.status(500).json({message: "Internal server error"});
   }
-
 });
 
-app.post('/api/rooms', async (req, res) => {
-
- 
+app.post("/api/rooms", async (req, res) => {
   try {
     const newRoom = req.body;
     const createdRoom = await Rooms.create(newRoom);
     res.status(201).json(createdRoom);
   } catch (error) {
-    console.error('Error creating room:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error creating room:", error);
+    res.status(500).json({error: "Internal server error"});
   }
 
-return res.status(403).send('Not authorized')
+  return res.status(403).send("Not authorized");
 });
 
-app.put('/api/update-room-dates/:roomNumber', async (req, res) => {
-
-  
-  const { roomNumber } = req.params;
-  const { checkInDate, checkOutDate, isbooked, totalPrice,  timeDifference } = req.body; 
+app.put("/api/update-room-dates/:roomNumber", async (req, res) => {
+  const {roomNumber} = req.params;
+  const {checkInDate, checkOutDate, isbooked, totalPrice, timeDifference} = req.body;
 
   try {
     const room = await Rooms.findOneAndUpdate(
-      { room: roomNumber },
-      { checkin: checkInDate, checkout: checkOutDate, isBooked: isbooked, price: totalPrice, numberOfDays:timeDifference }, 
-      { new: true } 
+        {room: roomNumber},
+        {checkin: checkInDate, checkout: checkOutDate, isBooked: isbooked, price: totalPrice, numberOfDays: timeDifference},
+        {new: true},
     );
-    
+
     if (!room) {
-      return res.status(404).json({ message: 'Room not found' });
+      return res.status(404).json({message: "Room not found"});
     }
 
-    res.json({ message: 'Room dates updated successfully', room });
+    res.json({message: "Room dates updated successfully", room});
   } catch (error) {
-    console.error('Error updating room dates:', error);  
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error updating room dates:", error);
+    res.status(500).json({message: "Internal server error"});
   }
-}); 
+});
 
 app.post("/create-checkout-session", async (req, res) => {
- 
   try {
-    const roomNumbers = req.body.items.map(item => item.price_data.product_data.roomNumber);
+    const roomNumbers = req.body.items.map((item) => item.price_data.product_data.roomNumber);
 
-    const roomInfoPromises = roomNumbers.map(async roomNumber => {
-      const room = await Rooms.findOne({ room: roomNumber });
+    const roomInfoPromises = roomNumbers.map(async (roomNumber) => {
+      const room = await Rooms.findOne({room: roomNumber});
       if (!room) {
         throw new Error(`Room with number ${roomNumber} not found.`);
       }
-      console.log('price is :',  room.price * 100,)
+      console.log("price is :", room.price * 100);
 
-      return {        
+      return {
         priceInCents: room.price * 100,
         roomNumber: room.room,
         roomType: room.roomType,
       };
     });
-    
+
     const roomInfo = await Promise.all(roomInfoPromises);
- 
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      line_items: req.body.items.map(item => {
-        const roomInfoItem = roomInfo.find(info =>
-          info.roomNumber === item.price_data.product_data.roomNumber
+      line_items: req.body.items.map((item) => {
+        const roomInfoItem = roomInfo.find((info) =>
+          info.roomNumber === item.price_data.product_data.roomNumber,
         );
         if (!roomInfoItem) {
           throw new Error(
-            `Room info not found for room number ${item.price_data.product_data.roomNumber}.`
+              `Room info not found for room number ${item.price_data.product_data.roomNumber}.`,
           );
         }
         return {
@@ -250,17 +233,15 @@ app.post("/create-checkout-session", async (req, res) => {
         };
       }),
       success_url: `${functions.config().client.url}/success`,
-      cancel_url: `${functions.config().client.url}/cancel`,  
+      cancel_url: `${functions.config().client.url}/cancel`,
     });
-    res.json({ url: session.url });
+    res.json({url: session.url});
   } catch (e) {
-    res.status(500).json({ error: e.message });
+    res.status(500).json({error: e.message});
   }
-
 });
-  
 
 
 app.listen(8000, () => {
-  console.log('server started');
+  console.log("server started");
 });
