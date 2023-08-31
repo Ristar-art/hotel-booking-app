621a2488752da6582851e3211001391bf499fdb5
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-08-31 12:26:13 +0200

    cammit chages

--- functions/models/hotel.models.js
+++ functions/models/hotel.models.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const HotelBookingSchema = new mongoose.Schema({
-  room: { type: Number, required: true, unique: true },
-  roomType: { type: String, required: true },
-  checkin: { type: Date, default: null },  
-  checkout: { type: Date, default: null },  
-  rentperday: { type: Number, required: true },
-  RoomPhoto: { type: String, required: true },
-  discription: { type: String, required: true },
-  isBooked: { type: Boolean },
-  price:{type: Number, default:null},
-  numberOfDays: {type:Number, default:null}
+  room: {type: Number, required: true, unique: true},
+  roomType: {type: String, required: true},
+  checkin: {type: Date, default: null},
+  checkout: {type: Date, default: null},
+  rentperday: {type: Number, required: true},
+  RoomPhoto: {type: String, required: true},
+  discription: {type: String, required: true},
+  isBooked: {type: Boolean},
+  price: {type: Number, default: null},
+  numberOfDays: {type: Number, default: null},
 },
-{ collection: 'rooms' }
+{collection: "rooms"},
 );
 
-const Rooms = mongoose.model('Rooms', HotelBookingSchema);
+const Rooms = mongoose.model("Rooms", HotelBookingSchema);
 module.exports = Rooms;
--- functions/models/user.models.js
+++ functions/models/user.models.js
@@ -1,17 +1,17 @@
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const User = new mongoose.Schema({
-    uid: {type:String},
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    passwordRepeat: { type: String, required: true },
-   
+  uid: {type: String},
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  passwordRepeat: {type: String, required: true},
+
 },
-{ collection: 'user-data' }
-)
+{collection: "user-data"},
+);
 
-const model = mongoose.model("UserData", User)
+const model = mongoose.model("UserData", User);
 
-module.exports = model
+module.exports = model;
