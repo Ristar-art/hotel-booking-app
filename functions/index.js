b16df736385922641ca45d658bf3bbbf913ed627
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-08-31 14:20:36 +0200

    changes commited

--- functions/index.js
+++ functions/index.js
@@ -23,13 +23,13 @@ app.use(decodeIDToken);
 const serviceAccount = require("./serviceAccount.json");
 
 
-
-mongoose.connect(functions.config().mongodb.uri, {
+//console.log("functions.config():", functions.config());
+mongoose.connect('mongodb+srv://boiketlo:wlAng9qDc0ZoIA8C@cluster0.jvdtm2c.mongodb.net/hotel?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const stripe = require("stripe")(functions.config().stripe.privatekey);
+const stripe = require("stripe")(sk_test_51NYoi5AndaV81xG1VJdCLTC9cRI0paqZz38AR9NE1TsOANlSR08dZsrxMoyyMLK9CMFXORBbf2PNmnXCLLtUTCQU00UhRj5nCb);
 
 const Rooms = require("./models/hotel.models");
 app.post("/api/signup", async (req, res) => {
