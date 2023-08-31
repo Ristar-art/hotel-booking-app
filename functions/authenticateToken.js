fa2bc9c8eed9de3e0799d9d4988796e2f6372a24
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-08-31 13:08:19 +0200

    dep added

---
+++ functions/authenticateToken.js
@@ -0,0 +1,34 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccount.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://booking-hotel-25ea1.firebaseio.com",
+});
+
+/**
+ * Middleware to decode Firebase ID token from request headers.
+ * If a valid ID token is present in the request headers, it is decoded
+ * and added to the request object as "currentUser".
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Next middleware function.
+ */
+async function decodeIDToken(req, res, next) {
+  const header = req.headers?.authorization;
+  const reqHeders = req.headers?.authorization?.startsWith("Bearer ");
+  const eq= "Bearer null" && reqHeders;
+  if (header !== eq) {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
+
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      req["currentUser"] = decodedToken;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  next();
+}
+
+module.exports = decodeIDToken;
