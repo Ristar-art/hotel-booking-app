d89ea97c034aa9e813290ecb067b7e696b48b4c5
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-10-23 09:13:02 +0200

    shema changed

--- Server/models/history.model.js
+++ Server/models/history.model.js
@@ -1,36 +1,17 @@
 const mongoose = require('mongoose');
 
-const historySchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  roomNumber: {
-    type: Number,
-    required: true
-  },
-  roomType: {
-    type: String,
-    required: true
-  },
-  checkInDate: {
-    type: Date,
-    required: true
-  },
-  checkOutDate: {
-    type: Date,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  numberOfDays: {
-    type: Number,
-    required: true
-  }
-});
+const HistoryBookingSchema = new mongoose.Schema({
+  // historyId: { type: String, required: true, unique: true },
+  email :{ type: String, required: true },
+  roomNumber: { type: Number, required: true, },
+  roomType: { type: String, required: true },
+  checkInDate: { type: Date, default: null },  
+  checkOutDate: { type: Date, default: null },
+  price:{type: Number, default:null},
+  numberOfDays: {type:Number, default:null}
+},
+{ collection: 'history' }
+);
 
-const History = mongoose.model('History', historySchema);
-
+const History = mongoose.model('History', HistoryBookingSchema);
 module.exports = History;
